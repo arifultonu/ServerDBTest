98fa7b96251d6ce5b9d16353d6081cd3c0c512b1
MD. ARIFUL ISLAM <tonu2050@gmail.com>
2016-01-19 18:20:28 +0600

    Server DB Test on Android Studio
    Test For send data to java server

---
+++ app/src/main/java/expenditure/arifultonu/com/serverdbtest/IPConfigure.java
@@ -0,0 +1,26 @@
+package expenditure.arifultonu.com.serverdbtest;
+
+public class IPConfigure {
+	public static String getIP() {
+
+	//return "http://202.40.178.58:80/"; // For Live //
+
+		//return "http://202.40.190.14:8085/"; //For Test
+		//
+
+		//Local IP and Server/Tomcat Port
+		return "http://10.11.201.27:80/"; //For Test
+
+		// return "http://10.0.2.2:8085/";// FOr own Pc
+
+		// private static String url_login =
+		// "http://10.0.2.2:8084/BankAndroidConnectivity/LoginMobiBank";
+	}
+
+/*	public static String getIP2() {
+
+		return "https://www.bankasia.net/"; // For Live //
+
+	}*/
+
+}
---
+++ app/src/main/java/expenditure/arifultonu/com/serverdbtest/MainActivity.java
@@ -0,0 +1,124 @@
+package expenditure.arifultonu.com.serverdbtest;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
+
+    private static String insert_url = IPConfigure.getIP()
+            + "ArifulTonuAndroidConnectivity/RegistrationServlet";
+
+
+    JSONParser jsonParser = new JSONParser();
+    JSONObject json;
+    JSONArray jsonArray = null;
+
+    EditText userName;
+    EditText password;
+    EditText emailId;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    userName = (EditText) findViewById(R.id.name);
+     password = (EditText) findViewById(R.id.password);
+       emailId = (EditText) findViewById(R.id.email);
+        Button btnSave = (Button) findViewById(R.id.btnSave);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String userName1=userName.getText().toString();
+                String pass=password.getText().toString();
+                String email=emailId.getText().toString();
+
+                new DataInsert().execute(userName1,pass,email);
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        return false;
+    }
+
+    private  class  DataInsert extends AsyncTask<String, Integer, String>{
+        String  randomString;
+
+        @Override
+        protected String doInBackground(String... args) {
+            //TelephonyManager mngr = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+           //String imei = mngr.getDeviceId();
+
+            String username = args[0],
+                    pass = args[1],
+                    email = args[2];
+
+//
+           // String unameM = randomString + uname.getText().toString();
+           // String email = randomString + email.getText().toString();
+            //String pass = randomString + pass.getText().toString();
+
+            List<NameValuePair> params = new ArrayList<NameValuePair>();
+            params.add(new BasicNameValuePair("username", username));
+            params.add(new BasicNameValuePair("pass", pass));
+            params.add(new BasicNameValuePair("email", email));
+           // params.add(new BasicNameValuePair("uname", userName.getText().toString()));
+           // params.add(new BasicNameValuePair("email", password.getText().toString()));
+
+
+           json = jsonParser.makeHttpRequest(insert_url, "POST", params);
+
+
+
+            return null;
+        }
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
